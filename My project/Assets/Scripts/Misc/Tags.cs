2139b86c35cb920be32d5f81d82018cfc3643022
2019715206 <154847839+2019715206@users.noreply.github.com>
2024-06-22 21:42:08 -0400

    drop item from inventory

--- My project/Assets/Scripts/Misc/Tags.cs
+++ My project/Assets/Scripts/Misc/Tags.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public static class Tags
 {
     public const string BoundsConfiner = "BoundsConfiner";
+    public const string ItemsParentTransform = "ItemsParentTransform";
 
 }
     
--- My project/Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
+++ My project/Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
@@ -1,13 +1,98 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UIInventorySlot : MonoBehaviour
+
+public class UIInventorySlot : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    private Camera mainCamera;
+    private Transform parentItem;
+        private GameObject draggedItem;
+
     public Image inventorySlotHighlight;
     public Image inventorySlotImage;
     public TextMeshProUGUI textMeshProUGUI;
 
+    [SerializeField] private UIInventoryBar inventoryBar = null;
     [HideInInspector] public ItemDetails itemDetails;
+    [SerializeField] private GameObject itemPrefab = null;
     [HideInInspector] public int itemQuantity;
+
+
+    private void OnDisable()
+    {
+        EventHandler.AfterSceneloadEvent -= SceneLoaded;
+    }
+
+    private void OnEnable()
+    {
+        EventHandler.AfterSceneloadEvent += SceneLoaded;
+    }
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void DropSelectedItemAtMousePosition()
+    {
+        if (itemDetails != null)
+        {
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCamera.transform.position.z));
+
+            GameObject itemGameObject = Instantiate(itemPrefab, worldPosition, Quaternion.identity, parentItem);
+            Item item = itemGameObject.GetComponent<Item>();
+            item.ItemCode = itemDetails.itemCode;
+
+            InventoryManager.Instance.RemoveItem(InventoryLocation.player, item.ItemCode);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (itemDetails != null)
+        {
+            Player.Instance.DisablePlayerInputAndResetMovement();
+
+            draggedItem = Instantiate(inventoryBar.inventoryBarDraggedItem, inventoryBar.transform);
+
+            Image draggedItemImage = draggedItem.GetComponentInChildren<Image>();
+            draggedItemImage.sprite = inventorySlotImage.sprite;
+        }
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (draggedItem != null)
+        {
+            draggedItem.transform.position = Input.mousePosition;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (draggedItem != null)
+        {
+            Destroy(draggedItem);
+
+            if(eventData.pointerCurrentRaycast.gameObject != null && eventData.pointerCurrentRaycast.gameObject.GetComponent<UIInventorySlot>() != null)
+            {
+
+            }
+            else
+            {
+                if (itemDetails.canBeDropped)
+                {
+                    DropSelectedItemAtMousePosition();
+                }
+            }
+            Player.Instance.EnablePlayerInput();
+        }
+    }
+
+    private void SceneLoaded()
+    {
+        parentItem = GameObject.FindGameObjectWithTag(Tags.ItemsParentTransform).transform;
+    }
+
+    
 }
